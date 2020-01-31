74513b8b56b22d289d4d7f6eabff1bef4f337916
ade rahmat nurdiyana <nurdiyana.ade@gmail.com>
2020-02-01 01:36:38 +0700

    Update mesin_m.php

--- application/models/mesin_m.php
+++ application/models/mesin_m.php
@@ -40,7 +40,19 @@ class Mesin_m extends CI_Model {
 		$this->Merk = $post['merk'];
 		$this->Tonase = $post['tonase'];
 		$this->db->insert($this->_table, $this);
+
 	}
+
+	public function update($post)
+	{
+		$post = $this->input->post();
+		$this->IdMesin = $post['id'];
+		$this->NoMc = $post['nomc'];
+		$this->Merk = $post['merk'];
+		$this->Tonase = $post['tonase'];
+		$this->db->update($this->_table, $this, array('IdMesin' => $post['id']));
+	}
+	
 	public function GetById($id)
 	{
 		return $this->db->get_where($this->_table,["IdMesin" => $id])->row();
